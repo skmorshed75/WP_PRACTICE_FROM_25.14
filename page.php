95d5b464d2ee9d82ce68f38442e98799985aea43
Sk Md Morshed <skmorshed75@yahoo.com>
2019-12-15 23:32:11 +0600

    Transient & its uses 25.19

--- page.php
+++ page.php
@@ -4,5 +4,13 @@
 get_header();
 the_post();
 the_content();
+//Class 25.19
+echo "<hr/>";
+echo get_transient('hello');
+echo "<hr/>";
+echo "<hr/>";
+echo get_transient('tran2');
+echo "<hr/>";
+//End of Class 25.19
 get_footer();
 ?>
